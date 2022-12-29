7d2db39eda69808fee6749fa6db2b47d4ea1b2e9
sus git <windows10@duck.com>
2022-12-29 17:51:36 +0700

    fixed

--- x11-packages/xf86-input-mtev/build.sh
+++ x11-packages/xf86-input-mtev/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/twaik/xorg-input-mtev
 TERMUX_PKG_DESCRIPTION="X.org mtev input driver"
 TERMUX_PKG_VERSION=1.0
 TERMUX_PKG_LICENSE=custom
+TERMUX_PKG_LICENSE_FILE=COPYING
 TERMUX_PKG_SRCURL=https://github.com/twaik/xorg-input-mtev/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=faecee79322b82c56b35422be3077f7c85a6d16d5ed8e3b3be809b7e765dd4ae
 TERMUX_PKG_DEPENDS="mtdev, xorg-server"
@@ -19,7 +20,6 @@ termux_step_make() {
         -shared \
         -Wl,-soname -Wl,mtev_drv.so \
         ${LDFLAGS} \
-        -lXFree86 \
         $(pkg-config --libs mtdev) \
         -o mtev_drv.so
 }
