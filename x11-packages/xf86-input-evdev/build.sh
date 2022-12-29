a3e18bfb93cabbb7654d11b2282420ba970bb3ef
sus git <windows10@duck.com>
2022-12-29 20:39:58 +0700

    no static

--- x11-packages/xf86-input-evdev/build.sh
+++ x11-packages/xf86-input-evdev/build.sh
@@ -8,7 +8,7 @@ TERMUX_PKG_LICENSE_FILE=COPYING
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/driver/xf86-input-evdev-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=8726073e81861bc7b2321e76272cbdbd33c7e1a121535a9827977265b9033ec0
 TERMUX_PKG_DEPENDS="libevdev, mtdev, xorg-server"
-
+TERMUX_PKG_NO_STATICSPLIT=false
 TERMUX_PKG_BLACKLISTED_ARCHES="i686"
 
 termux_step_pre_configure() {
