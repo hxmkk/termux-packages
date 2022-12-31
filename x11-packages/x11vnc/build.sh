b4bcb9e2ce1e9701f8ecaeac871f7221b8a920b9
sus git <windows10@duck.com>
2022-12-31 19:05:56 +0700

    x11vnc: mdns support

--- x11-packages/x11vnc/build.sh
+++ x11-packages/x11vnc/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION=0.9.16
 TERMUX_PKG_REVISION=10
 TERMUX_PKG_SRCURL=https://github.com/LibVNC/x11vnc/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=885e5b5f5f25eec6f9e4a1e8be3d0ac71a686331ee1cfb442dba391111bd32bd
-TERMUX_PKG_DEPENDS="libandroid-shmem, libcairo, libvncserver, libx11, libxcomposite, libxcursor, libxdamage, libxext, libxfixes, libxi, libxinerama, libxrandr, libxtst, openssl, xorg-xdpyinfo"
+TERMUX_PKG_DEPENDS="libandroid-shmem, libcairo, libvncserver, libx11, libxcomposite, libxcursor, libxdamage, libxext, libxfixes, libxi, libxinerama, libxrandr, libxtst, openssl, xorg-xdpyinfo, avahi"
 
 termux_step_pre_configure() {
 	autoreconf -vi
